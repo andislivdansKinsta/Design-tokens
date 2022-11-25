d50c4fbfe285972e2534a9f2f3ee6ee3247f8a4c
andislivdansKinsta <andislivdans@kinsta.com>
2022-11-25 13:19:22 +0200

    node install

---
+++ build/compose/StyleDictionarySize.kt
@@ -0,0 +1,41 @@
+
+
+// Do not edit directly
+// Generated on Fri, 25 Nov 2022 11:16:59 GMT
+
+
+
+package StyleDictionarySize;
+
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object StyleDictionarySize {
+  val size10xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val size3xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val size3xSmall = roundTo(roundTo(roundTo(roundTo(256.00.dp / 19.20.dp / 2,0) *2 / 19.20.dp / 2,0) *2 / 19.20.dp / 2,0) *2 / 19.20.dp /2,0) *2
+  val size4xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val size5xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val size6xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val size7xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val size8xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val size9xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  /** the base size of the font */
+  val sizeFontBase = 16.00.sp
+  /** the large size of the font */
+  val sizeFontLarge = 32.00.sp
+  /** the medium size of the font */
+  val sizeFontMedium = 16.00.sp
+  /** the small size of the font */
+  val sizeFontSmall = 12.00.sp
+  val sizeLarge = roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val sizeMedium = roundTo(256.00.dp * 19.20.dp /2,0) *2
+  val sizeRegular = 256.00.dp
+  val sizeScale = 19.20.dp
+  val sizeSmall = roundTo(256.00.dp / 19.20.dp / 2,0) *2
+  val sizeXLarge = roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val sizeXSmall = roundTo(roundTo(256.00.dp / 19.20.dp / 2,0) *2 / 19.20.dp / 2,0) *2
+  val sizeXxLarge = roundTo(roundTo(roundTo(roundTo(256.00.dp * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2 * 19.20.dp /2,0) *2
+  val sizeXxSmall = roundTo(roundTo(roundTo(256.00.dp / 19.20.dp / 2,0) *2 / 19.20.dp / 2,0) *2 / 19.20.dp / 2,0) *2
+}
---
+++ build/ios-swift/StyleDictionarySize.swift
@@ -0,0 +1,35 @@
+
+//
+// StyleDictionarySize.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 25 Nov 2022 11:16:59 GMT
+
+
+import UIKit
+
+public enum StyleDictionarySize {
+    public static let 10xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let 3xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let 3xSmall = roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) / CGFloat(19.20) / 2,0) *2 / CGFloat(19.20) / 2,0) *2 / CGFloat(19.20) / 2,0) *2 / CGFloat(19.20) /2,0) *2
+    public static let 4xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let 5xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let 6xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let 7xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let 8xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let 9xLarge = roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let fontBase = CGFloat(16.00) /* the base size of the font */
+    public static let fontLarge = CGFloat(32.00) /* the large size of the font */
+    public static let fontMedium = CGFloat(16.00) /* the medium size of the font */
+    public static let fontSmall = CGFloat(12.00) /* the small size of the font */
+    public static let large = roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let medium = roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2
+    public static let regular = CGFloat(256.00)
+    public static let scale = CGFloat(19.20)
+    public static let small = roundTo(CGFloat(256.00) / CGFloat(19.20) / 2,0) *2
+    public static let xLarge = roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let xSmall = roundTo(roundTo(CGFloat(256.00) / CGFloat(19.20) / 2,0) *2 / CGFloat(19.20) / 2,0) *2
+    public static let xxLarge = roundTo(roundTo(roundTo(roundTo(CGFloat(256.00) * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2 * CGFloat(19.20) /2,0) *2
+    public static let xxSmall = roundTo(roundTo(roundTo(CGFloat(256.00) / CGFloat(19.20) / 2,0) *2 / CGFloat(19.20) / 2,0) *2 / CGFloat(19.20) / 2,0) *2
+}
